9f9daacf9d118b3c9a79197009c043962de55c9c
KULDIP KUMAR SAH <61648038+kuldip11@users.noreply.github.com>
2024-11-23 23:18:52 +0530

    ADD: Adding FAQ Data

--- src/app/common/constant/index.tsx
+++ src/app/common/constant/index.tsx
@@ -19,3 +19,18 @@ West Bengal, India.
 🙏🏻`;
 
 export const whatsAppContactMsg = message;
+
+export const faqData = [
+  {
+    question: "Is there a free trial available?",
+    answer: `Yes, you can try us for free for 30 days. During this trial period, you will have access to all the features and benefits of our platform. 
+      This allows you to explore the full capabilities of our service, test its compatibility with your needs, and evaluate its overall performance without any commitment. 
+      If you decide it's the right fit for you, you can seamlessly transition to a paid plan at the end of the trial.`,
+  },
+  {
+    question: "Can I change my plan later?",
+    answer: `Yes, you can change your plan at any time based on your evolving needs. Our platform is designed to provide flexibility, ensuring that you can upgrade or downgrade your subscription effortlessly. 
+      Whether you need access to additional features, increased limits, or a plan that better fits your budget, the process is simple and straightforward. 
+      Additionally, our support team is always available to guide you through the transition and answer any questions.`,
+  },
+];
